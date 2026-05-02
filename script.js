80d14ac1449d1e2b36c2c2f924bbda55519e9962
mashudurrahman06 <masudnetit@gmail.com>
2026-05-03 02:01:19 +0600

    update ui

--- script.js
+++ script.js
@@ -1,27 +1,114 @@
-document.getElementById("login-btn").addEventListener("click", function (e) {
-    e.preventDefault(); // form submit বন্ধ রাখবে (important)
+let issues = [];
 
-    const correctUser = 'admin';
-    const correctPass = 'admin123';
+const carddiv = document.getElementById("card-section");
 
-    const userInput = document.getElementById("UserName");
-    const passInput = document.getElementById("password");
-    const msg = document.getElementById("invalid-alert");
+  carddiv.innerHTML = "";
 
-    const user = userInput.value;
-    const pass = passInput.value;
-    
-    if (user === correctUser && pass === correctPass) {
-        window.location.href = "index.html";
-    } else {
-            msg.classList.remove("hidden");
+  const loading = document.getElementById("loading");
+async function loadissues() {
+  try {
 
-            setTimeout(() => {
-                msg.classList.add("hidden")
-            },2000);
-          userInput.value= ""  ;
-          passInput.value= ""  ;
-          userInput.focus();
-            
+    loading.classList.remove("hidden")
+    carddiv.classList.add("hidden")
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const data = await res.json();
+
+    issues = data.data;
+
+    displayissues();
+
+     loading.classList.add("hidden")
+    carddiv.classList.remove("hidden")
+
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+}
+
+function displayissues() {
+  const carddiv = document.getElementById("card-section");
+
+  carddiv.innerHTML = "";
+
+  issues.forEach(issue => {
+
+    const card = document.createElement("div");
+
+    let borderClass = "border-2 border-gray-200";
+    let imgsrc = "";
+
+    // status logic
+    if (issue.status === "open") {
+      borderClass = "border-t-4 border-green-500";
+      imgsrc = "./assets/Open-Status.png";
+    } 
+    else if (issue.status === "closed") {
+      borderClass = "border-t-4 border-purple-500";
+      imgsrc = "./assets/Closed.png";
     }
+
+    // ✅ FIX: priority define
+    let priority = issue.priority?.toLowerCase();
+
+    let priorityBg = "bg-gray-100";
+
+    if (priority === "high") {
+      priorityBg = "bg-[#FEECEC] text-red-500";
+    } 
+    else if (priority === "medium") {
+      priorityBg = "bg-[#FFF6D1] text-[#F59E0B]";
+    } 
+    else if (priority === "low") {
+      priorityBg = "bg-gray-200 text-[#9CA3AF]";
+    }
+
+
+    //--lebel--//
+
+    const labelColors = {
+  bug: "bg-red-100 text-red-500",
+  "help wanted": "bg-yellow-100 text-yellow-600",
+  enhancement: "bg-green-100 text-green-600",
+  "good first issue": "bg-blue-100 text-blue-600"
+};
+
+let labelsHTML = "";
+
+issue.labels?.forEach(label => {
+  const color = labelColors[label.toLowerCase()] || "bg-gray-100 text-gray-500";
+
+  labelsHTML += `
+    <span class="px-2 py-1 rounded-[32px] font-medium text-[10px] mr-1 ${color}">
+      ${label}
+    </span>
+  `;
 });
+
+    card.className = `p-4 shadow bg-white mx-4 mt-8 rounded-lg ${borderClass}`;
+
+    card.innerHTML = `
+      <div class="flex justify-between items-center">
+        <img src="${imgsrc}" class="w-8 h-8" />
+        <span class="px-4 py-1 rounded-[32px] ${priorityBg}">
+          ${issue.priority}
+        </span>
+      </div>
+
+      <h2 class="font-semibold text-[14px] pb-2 pt-3">${issue.title}</h2>
+      <p class"text-[#64748B] text-[12px] ">${issue.description.slice(0,55)}...</p>
+
+        <div class="mt-2 pb-3 border-b-[1px] border-gray-400 flex flex-wrap gap-1">
+    ${labelsHTML}
+  </div>
+
+  <p class="text-[12px] text-gray-600 pt-3"> ${issue.author}</p>
+
+  <p class="text-[12px] text-gray-600 pt-3"> ${issue.createdAt}</p>
+      
+    `;
+
+    carddiv.appendChild(card);
+  });
+}
+
+loadissues();
